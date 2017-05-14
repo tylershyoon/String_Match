6b0bff2c1f7de78875da653e2c5bde9c1c97f248
tylershyoon <tylershyoon@gmail.com>
2017-05-15 04:51:10 +0900

    minor revision

--- problem1_b.py
+++ problem1_b.py
@@ -1,21 +1,23 @@
+import codecs
 
 def InputHandler():
-    pattern = open("pattern.txt", "r")
+    #pattern = codecs.open("pattern.txt", "r", "utf-8")
+    pattern = open("pattern.txt","r")
     P = "".join(pattern.readlines())
+    #raw = codecs.open("raw.txt", "r", "utf-8")
     raw = open("raw.txt", "r")
     T = "".join(raw.readlines())
+    print (T)
     pattern.close()
     raw.close()
     return T, P
-
-
 
 ''' BF stands for brute force '''
 def BF_string_match():
     T, P = InputHandler()
     n = len(T); m = len(P)
     returnlst = []
-    for s in range(0, n-m):
+    for s in range(0, n-m+1):
         equal = True
         i = 0
         while equal is True and i<=(m-1):
